9220ff1dcef84d5b65e22758c88800835e748927
Joaoov07 <joaovitorroliveira1803@gmail.com>
2026-08-10 09:30:45 -0300

    Área Triangulo

---
+++ Atividade/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	
+	float base, altura, area;
+	
+	printf("Digite o valor da base: \n");
+	scanf("%f", &base);
+	printf("Digite o valor da altura: \n");
+	scanf("%f", &altura);
+	area = (base * altura)/2;
+	
+	printf("A area do triangulo e: %0.2f", area);
+	return 0;
+}
