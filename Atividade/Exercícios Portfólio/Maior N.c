20aa1198a6c7ad64d9bc2b3d797e6d00e19932b8
Joaoov07 <joaovitorroliveira1803@gmail.com>
2026-08-31 07:58:16 -0300

    Update Maior N.c

--- Atividade/Exercícios Portfólio/Maior N.c
+++ Atividade/Exercícios Portfólio/Maior N.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main() {
+    int a, b, c;
 
-int main(int argc, char *argv[]) {
-	
-	int a,b,c;
-		printf("Insira os valor de A, B, C:");
-		scanf("%d %d %d", &a, &b, &c);
-		if(a<b && a>c){
-			printf("%d eh o maior\n", a);
-		}
-		if (b>a && b>c){
-			printf("%d eh o maior\n", b);
-		}
-		if (c>a && c>b){
-			printf("%d eh o maior\n", c);
-		}
-	return 0;
+    printf("Insira os valores de A, B e C: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a > b && a > c) {
+        printf("%d eh o maior.\n", a);
+    }
+    else if (b > a && b > c) {
+        printf("%d eh o maior.\n", b);
+    }
+    else if (c > a && c > b) {
+        printf("%d eh o maior.\n", c);
+    }
+    else if (a == b && a == c) {
+        printf("Os tres valores sao iguais.\n");
+    }
+    else if (a == b && a > c) {
+        printf("A e B sao os maiores e sao iguais: %d\n", a);
+    }
+    else if (a == c && a > b) {
+        printf("A e C sao os maiores e sao iguais: %d\n", a);
+    }
+    else if (b == c && b > a) {
+        printf("B e C sao os maiores e sao iguais: %d\n", b);
+    }
+
+    return 0;
 }
