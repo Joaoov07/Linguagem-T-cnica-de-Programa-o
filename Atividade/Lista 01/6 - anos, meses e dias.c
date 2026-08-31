eee886db7eaf966e43436d105ead0efa85fa77ff
Joaoov07 <joaovitorroliveira1803@gmail.com>
2026-08-31 09:12:14 -0300

    Add files via upload

---
+++ Atividade/Lista 01/6 - anos, meses e dias.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int dias, anos, meses;
+
+    printf("Digite a quantidade de dias: ");
+    scanf("%d", &dias);
+
+    anos = dias / 365;
+    dias = dias % 365;
+
+    meses = dias / 30;
+    dias = dias % 30;
+
+    printf("%d ano(s)\n", anos);
+    printf("%d mes(es)\n", meses);
+    printf("%d dia(s)\n", dias);
+
+    return 0;
+}
